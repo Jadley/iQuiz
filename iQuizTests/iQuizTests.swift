6261e059ecb136562e15470d08f1be3a6a1855a5
Jadley Felipe <jadley.felipe@pitang.com>
2026-01-21 23:19:46 -0300

    Initial Commit

---
+++ iQuizTests/iQuizTests.swift
@@ -0,0 +1,17 @@
+//
+//  iQuizTests.swift
+//  iQuizTests
+//
+//  Created by Jadley Felipe on 21/01/26.
+//
+
+import Testing
+@testable import iQuiz
+
+struct iQuizTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
